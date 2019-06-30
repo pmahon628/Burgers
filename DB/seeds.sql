acb0ed5ab01fc82dc7e430017bef91e66046cd90
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-06-29 22:20:00 -0400

    work

---
+++ DB/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cowboy Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Taco Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fancy Burger', true);
