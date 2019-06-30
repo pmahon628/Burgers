e101b293b9b618ff7a623c8c20056242db58f975
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-06-29 22:19:42 -0400

    work

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var router = express.Router();
 
-var burger = require('./models/burger.js');
+var burger = require('./Models/burger.js');
 
 router.get('/', function(req, res){
     burger.all(function(data){
