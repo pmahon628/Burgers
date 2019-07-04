ca9ae78e65d7db9317fa54a7a8bd4ad0267b9b59
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-07-03 22:56:15 -0400

    updated

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var router = express.Router();
 
-var burger = require('./Models/burger.js');
+var burger = require('../Models/burger.js');
 
 router.get('/', function(req, res){
     burger.all(function(data){
@@ -14,7 +14,7 @@ router.get('/', function(req, res){
      });
 });
 
-router.post('/api/cats' ,function(req, res){
+router.post('/api/burgers' ,function(req, res){
     burger.create([
        'name' 
     ],  [
@@ -24,7 +24,7 @@ router.post('/api/cats' ,function(req, res){
     });
 });
 
-router.put('/api/cats/:id' ,function(req, res){
+router.put('/api/burgers/:id' ,function(req, res){
     var condition = 'id = ' + req.params.id;
 
     console.log('condition', condition);
